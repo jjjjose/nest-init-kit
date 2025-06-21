eec928f67336409f7097a846f4522b6e9e97e634
jjjjose <jjjjoseignacio@gmail.com>
2025-06-21 01:34:16 -0400

    Add application constants for default connection and data source configuration

---
+++ src/shared/constants/app.constants.ts
@@ -0,0 +1,10 @@
+/**
+ * Application constants
+ * Constantes de la aplicación
+ *
+ * Global constants used throughout the application
+ * Constantes globales usadas en toda la aplicación
+ */
+
+export const DEFAULT_CONNECTION_ALIAS = 'default'
+export const DATA_SOURCE_DEFAULT = 'DATA_SOURCE_DEFAULT'
